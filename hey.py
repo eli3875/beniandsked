d3b99a36b62a1270843bc71539348bc2fd99502e
jbt <noya.weinberg6340@gmail.com>
2024-09-11 11:30:07 +0300

    firat

--- hey.py
+++ hey.py
@@ -1,4 +1,5 @@
 import pygame
+
 col = 50
 line = 25
 
@@ -12,7 +13,6 @@ for i in range(line):
     for j in range(col):
         playerboard[i].append(0)
 
-
 for i in playerboard:
     print(i)
 
@@ -23,26 +23,42 @@ Y = line * 20
 index_benni_row = 0
 index_benni_col = 0
 
-scrn = pygame.display.set_mode((X, Y))
+screen = pygame.display.set_mode((X, Y))
 
-#beni
+#beniimg
 pygame.display.set_caption('image')
 imp = pygame.image.load("pixil-frame-0.png").convert()
 
+screen.blit(imp, (index_benni_row, index_benni_col))
 
-scrn.blit(imp, (index_benni_row, index_benni_col))
 
-# paint screen one time
 pygame.display.flip()
-status = True
-while (status):
 
+working = True
+while (working):
 
     for i in pygame.event.get():
 
-
         if i.type == pygame.QUIT:
-            status = False
+            working = False
 
-# deactivates the pygame library
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        index_benni_row -= 1
+
+    if keys[pygame.K_RIGHT]:
+        index_benni_row += 1
+
+    if keys[pygame.K_UP]:
+        index_benni_col -= 1
+
+    if keys[pygame.K_DOWN]:
+        index_benni_col += 1
+    screen.fill("yellow")
+    screen.blit(imp, (index_benni_row, index_benni_col))
+    pygame.display.update()
+
+
+
 pygame.quit()
