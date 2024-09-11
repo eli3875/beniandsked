94b277f804e2e20f5ee60d49760f82f18c4985ef
git_test <eliyahu146@gmail.com>
2024-09-11 11:11:37 +0300

    adding screen

--- beni_and_sked.py
+++ beni_and_sked.py
@@ -1,2 +1,13 @@
 import pygame
+from hey import *
 pygame.init()
+for i in playerboard:
+    print(i)
+screen = pygame.display.set_mode((col * 20 ,line * 20))
+running = True
+while running:
+    for event in pygame.event.get():
+
+        # Check for QUIT event
+        if event.type == pygame.QUIT:
+            running = False
